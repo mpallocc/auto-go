cf4f3bc2989c2672b00712423e74fa064c81cb4a
isabella.grassucci <isabella.grassucci@gmail.com>
2022-03-21 12:57:07 +0100

    Changing namespace exports

---
+++ test_autogo.R
@@ -0,0 +1,18 @@
+library("devtools")
+#use_git(message = "Initial commit r-github")
+#use_r("autoGO.R")
+load_all()
+autoGO(list_of_genes = ,dbs = ,my_comparison = )
+#strsplit1("canecane","cane")
+check()
+check()
+use_git()
+
+
+#lazyloading su description
+setwd("")
+
+
+document()
+
+
