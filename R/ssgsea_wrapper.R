61ee23e2e87423d97db50a3812a8a61981a7b383
isabella.grassucci <isabella.grassucci@gmail.com>
2022-04-11 16:43:35 +0200

    try ssgsea with internal data

--- R/ssgsea_wrapper.R
+++ R/ssgsea_wrapper.R
@@ -40,7 +40,6 @@ ssgsea_wrapper <- function(norm_data = "results/deseq_vst_data.txt", MSigDB_name
     conversion <- conversion_ensembl
     all_genes_conversion <- conversion %>%
       textshape::column_to_rownames(loc = "ensembl_gene_id")
-    return(head(all_genes_conversion))
 
     norm_data <- merge(norm_data, all_genes_conversion, by = 0) %>%
       select(-Row.names)
