9d5dadd70067f4faad04a264173d0da2a3fa1e60
Fabio Ticconi <fabio.ticconi@gmail.com>
2022-11-04 17:45:47 +0100

    formatting all files, fixing heatmap issue, fixing NAMESPACE

--- R/choose_database.R
+++ R/choose_database.R
@@ -1,12 +1,11 @@
 #' @title choose_database
 #'
 #' @description It allows the user to choose the databases on which to perform the enrichment analysis. Either it returns all the possible databases or a subset of them.
-#' @param db_search (Default = NULL), is the string pattern to be matched against the list of enrichR databases. Any matching DBs will be returned. 
+#' @param db_search (Default = NULL), is the string pattern to be matched against the list of enrichR databases. Any matching DBs will be returned.
 #' @export
 
 
 choose_database <- function(db_search = NULL) {
-
   dbs_table <- listEnrichrDbs()
   dbs <- dbs_table$libraryName
 
